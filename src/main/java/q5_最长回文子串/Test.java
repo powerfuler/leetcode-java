18744338a99a9af8f514aed326e441cdff5e488d
dingjianmin <jm.ding@zuolin.com>
2021-04-30 18:37:18 +0800

    005:最长回文子串

---
+++ src/main/java/q5_最长回文子串/Test.java
@@ -0,0 +1,14 @@
+package q5_最长回文子串;
+
+/*
+*/
+
+public class Test {
+    public static void main(String[] args) {
+
+        long t1 = System.currentTimeMillis();
+//        ListNode listNode1 = new Solution2().addTwoNumbers(l2, l1);
+        long t2 = System.currentTimeMillis();
+
+    }
+}
