766315212d17c4d28a5ac2c21189f5ef1a50d18f
dingjianmin <jm.ding@zuolin.com>
2021-05-07 17:43:41 +0800

    006:Z字形变换

---
+++ src/main/java/q6_Z字形变换/Test.java
@@ -0,0 +1,14 @@
+package q6_Z字形变换;
+
+/*
+*/
+
+public class Test {
+    public static void main(String[] args) {
+
+        long t1 = System.currentTimeMillis();
+//        ListNode listNode1 = new Solution2().addTwoNumbers(l2, l1);
+        long t2 = System.currentTimeMillis();
+
+    }
+}
