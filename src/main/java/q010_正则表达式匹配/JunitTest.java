7c7c6e56988faeee3f46c7b14b1f4edec68cd08c
dingjianmin <jm.ding@zuolin.com>
2021-05-08 18:48:30 +0800

    010正则表达式匹配

---
+++ src/main/java/q010_正则表达式匹配/JunitTest.java
@@ -0,0 +1,17 @@
+package q010_正则表达式匹配;
+
+import org.junit.Test;
+
+public class JunitTest {
+
+    @Test
+    public void test0() {
+
+        long t4 = System.currentTimeMillis();
+//        new Solution2().addTwoNumbers(l1, l2);
+        long t5 = System.currentTimeMillis();
+
+//        CommonUtil.print(listNode);
+    }
+
+}
