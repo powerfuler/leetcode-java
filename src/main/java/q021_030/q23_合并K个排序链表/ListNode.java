371d27755ba30ba9827cb5a06aedd158fad6cb97
dingjianmin <jm.ding@zuolin.com>
2021-05-25 12:57:59 +0800

    021:合并两个有序链表

---
+++ src/main/java/q021_030/q23_合并K个排序链表/ListNode.java
@@ -0,0 +1,10 @@
+package q021_030.q23_合并K个排序链表;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/main/java/q021_030/q25_k个一组翻转链表/ListNode.java
@@ -0,0 +1,12 @@
+package q021_030.q25_k个一组翻转链表;
+
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
