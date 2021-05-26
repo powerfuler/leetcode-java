d3622c0ca85a70e01a2c80e843fb806fe7f92cc7
Eric <1173260643@qq.com>
2021-05-27 00:07:47 +0800

    025:K 个一组翻转链表

--- src/main/java/q1_3/JunitTest.java
+++ src/main/java/q1_3/JunitTest.java
@@ -1,17 +1,25 @@
 package q1_3;
 
+import entity.ListNode;
 import org.junit.Test;
+import q1_3.code.Solution1;
 
 public class JunitTest {
-
     @Test
     public void test0() {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(5);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
 
-        long t4 = System.currentTimeMillis();
-//        new Solution2().addTwoNumbers(l1, l2);
-        long t5 = System.currentTimeMillis();
-
-//        CommonUtil.print(listNode);
+        long t1 = System.currentTimeMillis();
+        ListNode listNode = new Solution1().reverseKGroup(n1, 2);
+        long t2 = System.currentTimeMillis();
+        System.out.println(listNode);
     }
-
 }
