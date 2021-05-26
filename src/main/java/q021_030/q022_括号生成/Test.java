ea983da634be4d0bdf4665c001083ed7042bc65b
dingjianmin <jm.ding@zuolin.com>
2021-05-26 12:20:55 +0800

    022:括号生成

--- src/main/java/q021_030/q022_括号生成/Test.java
+++ src/main/java/q021_030/q022_括号生成/Test.java
@@ -3,14 +3,14 @@ package q021_030.q022_括号生成;
 /*
 */
 
-import q021_030.q022_括号生成.code.Solution2;
+import q021_030.q022_括号生成.code.Solution;
 
 import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
         long t4 = System.currentTimeMillis();
-        List<String> strings = new Solution2().generateParenthesis(2);
+        List<String> strings = new Solution().generateParenthesis(2);
         long t5 = System.currentTimeMillis();
         System.out.println(strings);
     }
