16b41243f01a3603b60921c4c154b16cb52f1ba0
dingjianmin <jm.ding@zuolin.com>
2021-06-10 18:06:37 +0800

    038:外观数列

--- src/main/java/q01_1/Test.java
+++ src/main/java/q01_1/Test.java
@@ -3,13 +3,11 @@ package q01_1;
 /*
  */
 
-import q031_040.q038_外观数列.code.Solution4;
-
 public class Test {
     public static void main(String[] args) {
         long t1 = System.currentTimeMillis();
-        String str = new Solution4().countAndSay(5);
+//        String str = new Solution4().countAndSay(5);
         long t2 = System.currentTimeMillis();
-        System.out.println(str);
+//        System.out.println(str);
     }
 }
