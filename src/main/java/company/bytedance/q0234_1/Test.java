4ebc7eef5bea6957a1438cee62e117c7973e3e77
dingjianmin <jm.ding@zuolin.com>
2021-06-11 18:27:49 +0800

    bytedance:0234:回文链表

--- src/main/java/company/bytedance/q0234_1/Test.java
+++ src/main/java/company/bytedance/q0234_1/Test.java
@@ -3,11 +3,16 @@ package company.bytedance.q0234_1;
 /*
  */
 
+import company.bytedance.q0234_1.code.Solution1;
+import utils.structure.ListNode;
+
 public class Test {
     public static void main(String[] args) {
+        ListNode l1 = ListNode.createTestData("[2,4,3,4]");
         long t1 = System.currentTimeMillis();
-//        String str = new Solution4().countAndSay(5);
+        boolean palindrome = new Solution1().isPalindrome(l1);
         long t2 = System.currentTimeMillis();
-//        System.out.println(str);
+        System.out.println(palindrome);
+        ListNode.print(l1);
     }
 }
