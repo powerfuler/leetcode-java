7d4067b84db0d2e5f3e3a483f7c8825a81170770
dingjianmin <jm.ding@zuolin.com>
2021-06-18 15:34:15 +0800

    bytedance:069:x 的平方根

---
+++ src/main/java/company/bytedance/q069_1/JunitTest.java
@@ -0,0 +1,15 @@
+package company.bytedance.q069_1;
+
+import org.junit.Test;
+
+public class JunitTest {
+
+    @Test
+    public void test0() {
+
+        long t4 = System.currentTimeMillis();
+//        new Solution2().addTwoNumbers(l1, l2);
+        long t5 = System.currentTimeMillis();
+
+    }
+}
---
+++ src/main/java/company/bytedance/q069_1/code/Solution1.java
@@ -0,0 +1,9 @@
+package company.bytedance.q069_1.code;
+
+/**
+ *
+ *
+ */
+public class Solution1 {
+
+}
