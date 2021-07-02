595203fd94388309a4e3791463f2b94e0c6dbfaa
dingjianmin <jm.ding@zuolin.com>
2021-07-02 17:20:49 +0800

    bytedance:215:数组中的第K个最大元素

--- src/main/java/company/bytedance/q0215_1/JunitTest.java
+++ src/main/java/company/bytedance/q0215_1/JunitTest.java
@@ -1,6 +1,6 @@
 package company.bytedance.q0215_1;
 
-import company.bytedance.q0215_1.code.Solution1;
+import company.bytedance.q0215_1.code.Solution2;
 import org.junit.Test;
 
 public class JunitTest {
@@ -9,7 +9,7 @@ public class JunitTest {
     public void test0() {
         int[] nums1 = {1, 2, 3, 1, 5, 6};
         long t4 = System.currentTimeMillis();
-        int kthLargest = new Solution1().findKthLargest(nums1, 2);
+        int kthLargest = new Solution2().findKthLargest(nums1, 2);
         long t5 = System.currentTimeMillis();
         System.out.println(kthLargest);
     }
