cbc91ef7efba64f4127c8b516d6d06975abd1061
dingjianmin <jm.ding@zuolin.com>
2021-08-02 18:39:35 +0800

    bytedance:45:跳跃游戏 II

--- src/main/java/q041_050/q045_1/JunitTest.java
+++ src/main/java/q041_050/q045_1/JunitTest.java
@@ -6,8 +6,8 @@ import q041_050.q045_1.code.Solution;
 public class JunitTest {
     @Test
     public void test0() {
-        int[] nums = new int[]{2, 9, 1, 5, 3, 2, 2, 1, 1};
-        int jump = new Solution().jump(nums);
+        int[] nums = new int[]{2, 3, 1, 5, 3, 2, 2, 1, 1};
+        int jump = new Solution().jump1(nums);
         System.out.println(jump);
     }
 }
--- src/main/java/q041_050/q045_1/code/Solution.java
+++ src/main/java/q041_050/q045_1/code/Solution.java
@@ -8,12 +8,46 @@ package q041_050.q045_1.code;
 public class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
-        int[] f = new int[n]; 
+        int[] f = new int[n];
         for (int i = 1, j = 0; i < n; i++) {
             while (j + nums[j] < i)
                 j++;
             f[i] = f[j] + 1;
         }
         return f[n - 1];
+    }
+
+    public int jump1(int[] nums) {
+        int end = 0;
+        int maxPosition = 0;
+        int steps = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            //找能跳的最远的
+            maxPosition = Math.max(maxPosition, nums[i] + i);
+            //遇到边界，就更新边界，并且步数加一
+            if (i == end) {
+                end = maxPosition;
+                steps++;
+            }
+        }
+        return steps;
+    }
+
+    public int jump2(int[] nums) {
+        //要找的位置
+        int position = nums.length - 1;
+        int steps = 0;
+        //是否到了第 0 个位置
+        while (position != 0) {
+            for (int i = 0; i < position; i++) {
+                if (nums[i] >= position - i) {
+                    //更新要找的位置
+                    position = i;
+                    steps++;
+                    break;
+                }
+            }
+        }
+        return steps;
     }
 }
