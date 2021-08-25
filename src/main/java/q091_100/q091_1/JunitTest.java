b33e7d2ca7088554b02b641858c91c819ef317e8
dingjianmin <jm.ding@zuolin.com>
2021-08-25 15:04:21 +0800

    bytedance:91:解码方法

--- src/main/java/q091_100/q091_1/JunitTest.java
+++ src/main/java/q091_100/q091_1/JunitTest.java
@@ -1,4 +1,4 @@
-package q091_100.q091_2;
+package q091_100.q091_1;
 
 import org.junit.Test;
 import utils.structure.ListNode;
--- src/main/java/q091_100/q091_2/code/Solution1.java
+++
@@ -1,9 +0,0 @@
-package q091_100.q091_2.code;
-
-/**
- *
- *
- */
-public class Solution1 {
-
-}
