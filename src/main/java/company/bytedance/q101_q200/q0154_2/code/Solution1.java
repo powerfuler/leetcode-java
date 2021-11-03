6908b587f973f29342505406ce902504f94b40db
dingjianmin <jm.ding@zuolin.com>
2021-11-03 17:40:16 +0800

    bytedance:154:寻找旋转排序数组中的最小值 II

--- src/main/java/company/bytedance/q101_q200/q0154_2/code/Solution1.java
+++ src/main/java/company/bytedance/q101_q200/q0154_2/code/Solution1.java
@@ -5,5 +5,24 @@ package company.bytedance.q101_q200.q0154_2.code;
  *
  */
 public class Solution1 {
+    public int findMin(int[] nums) {
+        int n = nums.length;
+        int l = 0, r = n - 1;
+        while (l < r && nums[0] == nums[r]) r--;
+        while (l < r) {
+            int mid = l + r + 1 >> 1;
+            if (nums[mid] >= nums[0]) {
+                l = mid;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return r + 1 < n ? nums[r + 1] : nums[0];
+    }
 
+    public static void main(String[] args) {
+        int[] ss = new int[]{2, 2, 2, 0, 1, 2};
+        int min = new Solution1().findMin(ss);
+        System.out.println(min);
+    }
 }
