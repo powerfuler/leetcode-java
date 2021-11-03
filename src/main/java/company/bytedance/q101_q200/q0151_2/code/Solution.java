aa87dfec9b2907b5b6c6f35a59bc3f11982651cc
dingjianmin <jm.ding@zuolin.com>
2021-11-03 16:15:49 +0800

    bytedance:151:翻转字符串里的单词

---
+++ src/main/java/company/bytedance/q101_q200/q0151_2/code/Solution.java
@@ -0,0 +1,24 @@
+package company.bytedance.q101_q200.q0151_2.code;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * 使用Java内置方法
+ * 1、先切分
+ * 2、逆序
+ * 3、输出
+ */
+class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.trim().split(" +");
+        Collections.reverse(Arrays.asList(words));
+        return String.join(" ", words);
+    }
+
+    public static void main(String[] args) {
+        String s = "  Bob    Loves  Alice   ";
+        String stringBuilder = new Solution().reverseWords(s);
+        System.out.println(stringBuilder);
+    }
+}
