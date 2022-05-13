d7fa7bcf70f8623074999fbca82cdcfb125f324c
dingjianmin <jm.ding@zuolin.com>
2022-05-13 10:38:28 +0800

    4

--- src/main/java/practice2/q004_寻找两个正序数组的中位数/Solution.java
+++ src/main/java/practice2/q004_寻找两个正序数组的中位数/Solution.java
@@ -22,44 +22,46 @@ public class Solution {
 
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         // 使num1成为较短数组,不仅可以提高检索速度,同时可以避免一些边界问题
-        if(nums1.length> nums2.length){
+        if (nums1.length > nums2.length) {
             int[] tem = nums1;
             nums1 = nums2;
             nums2 = tem;
         }
-        int len1 = nums1.length,len2 = nums2.length;
+        int len1 = nums1.length, len2 = nums2.length;
         // 两数组合并&排序后,左半边的长度
-        int leftLen = (len1+len2+1)/2;
+        int leftLen = (len1 + len2 + 1) / 2;
         // 对数组1进行二分检索
-        int start = 0 ,end = len1;
+        int start = 0, end = len1;
 //        int[] nums1 = new int[]{3, 4, 15, 18};
 //        int[] nums2 = new int[]{2, 6, 7, 9, 11, 13, 15};
-        while (start <= end){
+        while (start <= end) {
             // 找到两数组被测数A,B位置，两数组中位数，count1 = 2 表示 num1 数组的第2个数字,索引下标为1
-            int count1 = start + (end-start)/2;
-            int count2 = leftLen-count1;
+            int count1 = start + (end - start) / 2;
+            int count2 = leftLen - count1;
+
+            if (count1 > 0 && nums1[count1 - 1] > nums2[count2]) {
+                // A比B的next还要大
+                end = count1 - 1;
+            } else if (count1 < len1 && nums2[count2 - 1] > nums1[count1]) {
+                // B比A的next还要大
+                start = count1 + 1;
+            } else {
+                // 获取中位数
+                int result = (count1 == len1) ? nums2[count2 - 1] : // 当num1数组的数都在总数组右边
+                             (count2 == len2) ? nums1[count1 - 1] : // 当num2数组的数都在总数组右边
+                 Math.max(nums1[count1 - 1], nums2[count2 - 1]); //// 比较A,B
+
+                if (((len1 + len2) & 1) == 1) {
+                    return result;
+                }
+                // 处理偶数
+                int nextValue = (count1 == len1) ? nums2[count2] : // 当num1数组的数都在总数组右边
+                        (count2 == len2) ? nums1[count1] : // 当num2数组的数都在总数组右边
+                                Math.min(nums1[count1], nums2[count2]); //// 比较A,B
+                return (result + nextValue) / 2.0;
+            }
 
         }
-
+        return Integer.MIN_VALUE;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
